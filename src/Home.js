4687c32af67857bfbf6b2574c2414a93cda0da5f
Rizaldi Kartodijoyo <rizalkartodijoyo@gmail.com>
2020-07-12 12:45:04 +0900

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {} from "react-router-dom";
+
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
+export default () => <h1 styles={styles}>Magic happens in</h1>;
